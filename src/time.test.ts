25ab99ca3ca3be3935f31628a0c6b3c0884e96ea
nju33 <nju33.ki@gmail.com>
2020-07-27 13:46:56 +0900

    fix: rename diffTime to diffTimeFormat

--- src/time.test.ts
+++ src/time.test.ts
@@ -1,14 +1,14 @@
-import { diffTime } from './time'
+import { diffTimeFormat } from './time'
 
-describe('diffTime', () => {
+describe('diffTimeFormat', () => {
   test('time', () => {
     const endTs = new Date('2020-01-02:12:43:01.999Z').getTime()
     const startTs = new Date('2020-01-01:19:59:35.058Z').getTime()
 
-    expect(diffTime(endTs, startTs)).toBe('16:43:26.060')
+    expect(diffTimeFormat(endTs, startTs)).toBe('16:43:26.060')
   })
 
   test('throw', () => {
-    expect(() => diffTime(1, 2)).toThrow(TypeError)
+    expect(() => diffTimeFormat(1, 2)).toThrow(TypeError)
   })
 })
--- src/time.ts
+++ src/time.ts
@@ -3,10 +3,13 @@ const ONE_SECOND = 1000 * ONE_MILLISECOND
 const ONE_MINUTE = 60 * ONE_SECOND
 const ONE_HOUR = 60 * ONE_MINUTE
 
+export type DiffTimeFormatKinds = 'HH' | 'HH:mm' | 'HH:mm:ss' | 'HH:mm:ss.SSS'
+
 /**
  *
  * @param endTs - a timestamp
  * @param startTs - a timestamp
+ * @param format - output format
  * @returns `hh:MM:ss.SSS` syntax time
  *
  * @example
@@ -15,16 +18,20 @@ const ONE_HOUR = 60 * ONE_MINUTE
  * // If endTs is in the past, an error occurs
  * const endTs = new Date('2020-01-01:19:59:35.058Z').getTime()
  * const startTs = new Date('2020-01-02:12:43:01.999Z').getTime()
- * diffTime(endTs, startTs)
+ * DiffTimeFormat(endTs, startTs)
  * // TypeError: endTs should be in the future rather than startTs
  *
  * // The following is correct
  * const endTs = new Date('2020-01-02:12:43:01.999Z').getTime()
  * const startTs = new Date('2020-01-01:19:59:35.058Z').getTime()
- * diffTime(endTs, startTs)
+ * DiffTimeFormat(endTs, startTs)
  * ```
  */
-export function diffTime(endTs: number, startTs: number): string {
+export function diffTimeFormat(
+  endTs: number,
+  startTs: number,
+  format: DiffTimeFormatKinds = 'HH:mm:ss.SSS'
+): string {
   if (startTs > endTs) {
     throw new TypeError('`endTs` should be in the future rather than `startTs`')
   }
@@ -33,14 +40,23 @@ export function diffTime(endTs: number, startTs: number): string {
 
   const diffHours = diffTimeByTs / ONE_HOUR
   const diffHoursByString = `00${Math.floor(diffHours)}`.slice(-2)
+  if (format === 'HH') {
+    return diffHoursByString
+  }
 
   const diffMinutes =
     (ONE_HOUR * (diffHours - Math.floor(diffHours))) / ONE_MINUTE
   const diffMinuteByString = `00${Math.floor(diffMinutes)}`.slice(-2)
+  if (format === 'HH:mm') {
+    return `${diffHoursByString}:${diffMinuteByString}`
+  }
 
   const diffSeconds =
     (ONE_MINUTE * (diffMinutes - Math.floor(diffMinutes))) / ONE_SECOND
   const diffSecondByString = `00${Math.floor(diffSeconds)}`.slice(-2)
+  if (format === 'HH:mm:ss') {
+    return `${diffHoursByString}:${diffMinuteByString}:${diffSecondByString}`
+  }
 
   const diffMseconds =
     (ONE_SECOND * (diffSeconds - Math.floor(diffMinutes))) / ONE_MILLISECOND
@@ -48,3 +64,8 @@ export function diffTime(endTs: number, startTs: number): string {
 
   return `${diffHoursByString}:${diffMinuteByString}:${diffSecondByString}.${diffMsecondByString}`
 }
+
+/**
+ * @deprecated Use `diffTimeFormat` instead
+ */
+export { diffTimeFormat as diffTime }
